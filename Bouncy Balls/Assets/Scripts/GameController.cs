c4133e55f96eddc144827f5b33709a23da4f27bb
Dantrix555 <35179175+Dantrix555@users.noreply.github.com>
2019-09-09 23:28:53 -0500

    Main game added
    The game is ready to use or modify ;)

---
+++ Bouncy Balls/Assets/Scripts/GameController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject _point;
+    public GameObject _10points;
+    public GameObject _20points;
+    public Text _scoreText;
+    public Text _timeText;
+    public Text _gameOverText;
+    public int _totalScore;
+    public bool _gameOver;
+    private int _randomGenerator;
+    private int _pointsAmount;
+    private float _positionX;
+    private float _positionY;
+    private float _time;
+
+    void Start()
+    {
+        _time = 60;
+        _pointsAmount = 0;
+        //Update the score text to zero points
+        _totalScore = 0;
+        UpdateTexts(_scoreText, "Score: " + _totalScore);
+        //Spawn 20 random points Game Objects
+        int i;
+        for(i = 0; i < 20; i++)
+        {
+            //Set the random value to determine which point game object will be spawned
+            _randomGenerator = Random.Range(1,5);
+            //Set the starting positions of the point game object
+            _positionX = Random.Range(-7, 7);
+            _positionY = Random.Range(-3.2f, 3.2f);
+            GameObject _instatiatedPoint;
+            //Instanciate the game object and attach this script to the points controller script
+            if(_randomGenerator == 1)
+            {
+                _instatiatedPoint = Instantiate(_20points, new Vector2(_positionX, _positionY), Quaternion.identity);
+            }    
+            else if(_randomGenerator == 2)
+            {
+                _instatiatedPoint = Instantiate(_10points, new Vector2(_positionX, _positionY), Quaternion.identity);
+            }  
+            else
+            {
+                _instatiatedPoint = Instantiate(_point, new Vector2(_positionX, _positionY), Quaternion.identity);
+            }
+            _instatiatedPoint.GetComponent<PointsController>()._gameController = this;
+            _pointsAmount++;
+        }
+    }
+
+    void Update()
+    {
+        //Update Time
+        if(_time > 0 && !_gameOver)
+        {
+            UpdateTime();
+        }
+        //If time's out or player hit all point set the game over screen
+        if(_pointsAmount <= 0 || _time <= 0)
+        {
+            SetGameOver(true);
+        }
+        //If game over text is active the player can press R button to restart game
+        if (_gameOverText.IsActive() && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        //Player can quit whenever it wants to
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+    }
+
+    void SetGameOver(bool _gameOver)
+    {
+        _gameOverText.gameObject.SetActive(_gameOver);
+        this._gameOver = _gameOver;
+    }
+
+    void UpdateTime()
+    {
+        _time -= Time.deltaTime;
+        UpdateTexts(_timeText, "Time: " + (int)_time);
+    }
+
+    public void UpdateTexts(Text _textComponent, string _text)
+    {
+        _textComponent.text = _text;
+    }
+
+    public void UpdatePointsAmount()
+    {
+        _pointsAmount--;
+    }
+}
---
+++ Bouncy Balls/Assets/Scripts/PointsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsController : MonoBehaviour
+{
+    public int _score;
+    public int _rangeValue;
+    public GameController _gameController;
+    private float _xDirection;
+    private float _yDirection;
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        //Determines the direction of the point and set the velocity that will takes
+        _xDirection = Random.Range(-_rangeValue, _rangeValue);
+        _yDirection = Random.Range(-_rangeValue, _rangeValue);
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = new Vector2(_xDirection, _yDirection);
+    }
+
+    void OnMouseOver()
+    {
+        //Detect if the point is clicked with the game active
+        if (Input.GetMouseButtonDown(0) && !_gameController._gameOver)
+        {
+            //Set the score, update the text component and destroy the gameObject clicked
+            _gameController._totalScore += _score;
+            _gameController.UpdateTexts(_gameController._scoreText, "Score: " + _gameController._totalScore);
+            _gameController.UpdatePointsAmount();
+            Destroy(gameObject);
+        }
+    }
+}
