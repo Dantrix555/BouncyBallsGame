225a743139319d04fbb4468d9fbd6da32e3bffe0
Dantrix555 <suarezochoa98@gmail.com>
2021-02-11 22:11:22 -0500

    Android build fix 2

--- Bouncy Balls/Assets/Editor/JenkinsBuild.cs
+++ Bouncy Balls/Assets/Editor/JenkinsBuild.cs
@@ -55,7 +55,7 @@ public class JenkinsBuild
         var args = FindArgs();
 
         string fullPathAndName = args.targetDir + args.appName;
-        BuildProject(EnabledScenes, fullPathAndName, BuildTargetGroup.Android, BuildTarget.Android, BuildOptions.None);
+        BuildProject(EnabledScenes, fullPathAndName, BuildTargetGroup.Android, BuildTarget.Android, BuildOptions.AcceptExternalModificationsToPlayer);
     }
     
     // ------------------------------------------------------------------------
